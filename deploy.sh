f156bc834b1518209e5ab1af7fd79e73539b30c3
mattslaney <mattslaney@users.noreply.github.com>
2024-05-02 15:04:50 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,55 @@
 #!/usr/bin/env bash
 
-rm -r ./dist
-rm -r ./wasm/pkg
-rm -r ./wasm/target
-rm -r ./site/target
+clean() {
+  rm -r ./dist
+  rm -r ./wasm/pkg
+  rm -r ./wasm/target
+  rm -r ./site/target
+}
 
-cd wasm
-wasm-pack build --target web
-cd ..
+build_wasm() {
+  cd wasm
+  wasm-pack build --target web
+  cd ..
+}
 
-cd site/src
-tsc
-cp -r *.html ../target/
-cp -r *.css ../target/
-cd ../..
+build_site() {
+  cd site/src
+  tsc
+  cp -r ../../wasm/pkg ./pkg
+  cp -r *.html ../target/
+  cp -r *.css ../target/
+  cd ../..
+}
 
-cp -r ./site/target/ ./dist/
-cp -r ./wasm/pkg/ ./dist/pkg/
+deploy() {
+  cp -rf ./site/target/ ./dist/
+  cp -rf ./wasm/pkg/ ./dist/pkg/
+}
+
+if [[ $# -eq 0 ]]; then
+  clean
+  build_wasm
+  build_site
+  deploy
+else
+  for arg in "$@"; do
+    case $arg in
+      "-wasm")
+        rm -r ./wasm/pkg
+        rm -r ./wasm/target
+        build_wasm
+        ;;
+      "-site")
+        rm -r ./site/target
+        build_site
+        ;;
+      *)
+        echo "Invalid argument: $arg"
+        ;;
+    esac
+    deploy
+  done
+fi
 
 echo "Done!"
--- site/src/index.ts
+++
@@ -1,8 +0,0 @@
-function createHeading(text: string): void {
-    const heading = document.createElement("h1");
-    heading.textContent = text;
-    document.body.appendChild(heading);
-}
-
-createHeading("Hello, World!");
-
