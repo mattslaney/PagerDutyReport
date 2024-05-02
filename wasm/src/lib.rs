a3d5676d76f1fcfdb62c0bd0f14677607a5e1400
mattslaney <mattslaney@users.noreply.github.com>
2024-05-02 13:26:53 +0100

    .

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,10 +3,17 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn log_msg(msg: &str) {
+    log(msg);
 }
 
